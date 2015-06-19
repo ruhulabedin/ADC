f33db678a8d0ca7c2e13bb3b7e0318abd121cdd9
Ruhul Abedin <ruhul.abedin@gmail.com>
2015-06-18 22:02:40 -0700

    add a for loop in program

--- firstProgram.java
+++ firstProgram.java
@@ -1,5 +1,8 @@
 public class firstProgram{
  public static void main(String args[]){
  System.out.println("welcome git");
+for(int i=0;i<5;i++){
+System.out.println("second commit and pust in git");
+}
 }
 }
